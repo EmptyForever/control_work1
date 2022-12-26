276bfcb295a91b14e0b5aa9c79400d14d4bb4305
Aleks <kakdela208@mail.ru>
2022-12-26 14:14:56 +1000

    добавили метод для нахождения кол-ва строк

--- itog_task/Program.cs
+++ itog_task/Program.cs
@@ -1,2 +1,13 @@
-﻿int size = 2;
-Console.WriteLine(size);
+﻿Console.Clear();
+
+// метод для нахождения кол-ва строк, размер которых не больше 3
+int HowManyStringMore3(string[] arr1)
+{
+    int k = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        if (arr1[i].Length <= 3) k++;
+    }
+    return k;
+}
+
