53836d16de88d876b01b6262cae161341650a684
Aleks <kakdela208@mail.ru>
2022-12-26 14:18:35 +1000

    добавили метод для печати массива; написали код для создания массива с клавиатуры; и самое решение; complete cotrol_work END Thank you)

--- itog_task/Program.cs
+++ itog_task/Program.cs
@@ -27,3 +27,50 @@ string[] FillArr(string[] arr1, int size)
     return arr2;
 }
 
+// метод для печати массива
+void PrintArray(string[] arr)
+{
+    if (arr.Length == 0) 
+    {
+        Console.Write("[");
+        Console.Write("]");
+        return;
+    }
+    Console.Write("[");
+    int last = 0;
+    if (arr.Length > 1)
+    {
+        for (int i = 0; i <= arr.Length - 2; i++)
+        {
+            Console.Write($"{arr[i]}, ");
+            last = i;
+        }
+        last = last + 1;
+        Console.Write(arr[last]);
+    }
+    else Console.Write(arr[last]);
+    Console.Write("]");
+}
+
+Console.WriteLine("Введите размер будущегом ассива: ");
+int size1 = Convert.ToInt32(Console.ReadLine());
+
+// создаём массив из size1 элементов
+string[] array1 = new string[size1]; 
+for (int i = 0; i < array1.Length; i++)
+{
+    Console.Write($"Задайте строку {i + 1}: \n");
+    array1[i] = Console.ReadLine();
+    if (array1[i] == string.Empty)
+    {
+        array1[i] = "-";
+    }
+}
+
+int howMany = HowManyStringMore3(array1);
+Console.WriteLine($"{howMany}");
+string[] newArray = FillArr(array1, howMany);
+Console.WriteLine("Исходный массив строк: ");
+PrintArray(array1);
+Console.WriteLine($"\n\nНовый массив размером --> ({howMany}) \n с элементами длинной меньше 4: ");
+PrintArray(newArray);
